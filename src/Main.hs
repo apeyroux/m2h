3c6fe793c823799ec13580bcf198270cbdc2f3d7
Alexandre Peyroux <alex@px.io>
2019-06-16 23:01:01 +0200

    sample servant-client

--- src/Main.hs
+++ src/Main.hs
@@ -7,12 +7,16 @@
 
 module Main where
 
+import           Control.Concurrent
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BSLC
 import           GHC.Generics
+import           Network.HTTP.Client (newManager, defaultManagerSettings)
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
+import           Servant.Client
+import           System.Environment
 
 data M2H = M2H {
   m2hHours :: Integer
@@ -25,6 +29,12 @@ instance ToJSON M2H where
     object ["hours" .= h
            , "minutes" .= m
            , "initial" .= b]
+
+instance FromJSON M2H where
+  parseJSON = withObject "M2H" $ \v -> M2H
+        <$> v .: "hours"
+        <*> v .: "minutes"
+        <*> v .: "initial"
 
 instance MimeRender PlainText M2H where
   mimeRender _ val = BSLC.pack $ "In "
@@ -47,5 +57,19 @@ pApi = Proxy
 app :: Application
 app = serve pApi srv
 
+convert' :: Integer -> ClientM M2H
+convert' = client pApi
+
 main :: IO ()
-main = run 8081 app
+main = do
+  args <- getArgs
+  case args of
+    [nb] -> do
+      t <- forkIO $ run 8383 app
+      manager' <- newManager defaultManagerSettings
+      res <- runClientM (convert' (read nb::Integer)) (mkClientEnv manager' (BaseUrl Http "localhost" 8383 ""))
+      case res of
+        Right m -> putStrLn $ show (m2hHours m) <> " hour(s) " <> show (m2hMinutes m) <> " minute(s)"
+        Left err -> print err
+      killThread t
+    _ -> putStrLn "usage: m2h <minutes>"
